c9c72db253034bed83e7eb2d59c8ca6f5780edd3
Vasile Bogdan <wbogdan15@gmail.com>
2023-09-10 16:49:01 +0300

    Clean start

--- src/DirectoryTree.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <LUT/Tree.hpp>
-#include <functional>
-#include <boost/filesystem.hpp>
-
-lut::Tree buildDirectoryTree(
-	const std::string&, 
-	const std::function<std::string(const boost::filesystem::path&)>&);
